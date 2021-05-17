4531de6a14d796e8795d73e426e5aa31027d6e20
Raj Aryan Agrawal <rajaryanagrawal@Rajs-MacBook-Air.local>
2021-05-17 17:27:44 +0530

    compile on OSX now, override missing in openglframebuffer

--- Light/include/core/base.hpp
+++ Light/include/core/base.hpp
@@ -11,10 +11,14 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <unordered_map>
 
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
 #include "glm/gtx/string_cast.hpp"
+
+#include "glad/glad.h"
+
 
 #define BIT(x) (1 << x)
 #define BIND_EVENT_FN(x) std::bind(&x, this, std::placeholders::_1)
