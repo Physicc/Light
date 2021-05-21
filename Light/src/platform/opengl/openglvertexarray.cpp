432dfc00e049c48bf6dbac2068cdfa39c5ecd098
Tirthankar Mazumder <mztirthankar@gmail.com>
2021-05-22 00:00:34 +0530

    Made the destructor declaration a one-liner.

--- Light/src/platform/opengl/openglvertexarray.cpp
+++ Light/src/platform/opengl/openglvertexarray.cpp
@@ -35,8 +35,7 @@ namespace Light
 		glGenVertexArrays(1, &rendererId);
 	}
 	
-	OpenGLVertexArray::~OpenGLVertexArray() 
-	= default;
+	OpenGLVertexArray::~OpenGLVertexArray() = default;
 	
 	void OpenGLVertexArray::bind() const
 	{
