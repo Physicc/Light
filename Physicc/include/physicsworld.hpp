24ec7be0cd23701029efbc466b45d204605ecb29
Divyansh Tiwari <divyanshtiwari08@gmail.com>
2021-06-09 16:19:56 +0530

    added a gravity member to PhysicsWorld with setters and getters

--- Physicc/include/physicsworld.hpp
+++ Physicc/include/physicsworld.hpp
@@ -1,10 +1,21 @@
 #ifndef __PHYSICC_H__
 #define __PHYSICC_H__
-#include "../../Light/libs/glm/glm/fwd.hpp"
+#include "../../Light/libs/glm/glm/glm.hpp"
 
 class PhysicsWorld
 {
-    protected:
+    public:
+    PhysicsWorld(glm::vec3 gravity = glm::vec3(0,0,0));
+    void setgravity(glm::vec3);
+    glm::vec3 getgravity();
+    
+    
+    
+    
+    
+    private:
+    glm::vec3 m_gravity;
+    
 
     
 };
--- Physicc/src/physicsworld.cpp
+++ Physicc/src/physicsworld.cpp
@@ -1,2 +1,16 @@
 #include "physicsworld.hpp"
 
+
+PhysicsWorld::PhysicsWorld(glm::vec3 gravity)
+    :m_gravity(gravity)
+    {
+
+    }
+
+void PhysicsWorld::setgravity(glm::vec3 gravity){
+    m_gravity=gravity;
+}
+
+glm::vec3 PhysicsWorld::getgravity(){
+    return m_gravity;
+}
