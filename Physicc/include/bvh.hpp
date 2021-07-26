12ddc20b5fe2fb973edb32306ecd64f860d4ece9
Tirthankar Mazumder <63574588+wermos@users.noreply.github.com>
2021-07-26 11:31:28 +0530

    Created the BVH related source files.

---
+++ Physicc/include/bvh.hpp
@@ -0,0 +1,9 @@
+#ifndef __BVH_H__
+#define __BVH_H__
+
+class BVH
+{
+
+};
+
+#endif //__BVH_H__
---
+++ Physicc/include/bvhnode.hpp
@@ -0,0 +1,9 @@
+#ifndef __BVHNODE_H__
+#define __BVHNODE_H__
+
+class BVHNode
+{
+
+};
+
+#endif //__BVHNODE_H__
