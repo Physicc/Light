2773ea6da30e9c5f3ed27b7af622e057a7ab6517
Tirthankar Mazumder <63574588+wermos@users.noreply.github.com>
2021-07-26 11:49:18 +0530

    Added a few things to the BoundingVolume class.

--- Physicc/include/boundingvolume.hpp
+++ Physicc/include/boundingvolume.hpp
@@ -1,9 +1,31 @@
 #ifndef __BOUNDINGVOLUME_H__
 #define __BOUNDINGVOLUME_H__
 
-class BoundingVolume
-{
+#include "glm/glm.hpp"
 
-};
+namespace Physicc
+{
+	/**
+	 * @brief Axis Aligned Bounding Box
+	 *
+	 * Helper class to store diagonally opposite points of the AABB
+	 *
+	 */
+	struct AABB
+	{
+		glm::vec3 lowerBound;
+		glm::vec3 upperBound;
+	};
+
+	template<class T>
+	class BoundingVolume
+	{
+		public:
+			bool overlapsWith(BoundingVolume<T> v);
+
+		private:
+			T m_volume;
+	};
+}
 
 #endif //__BOUNDINGVOLUME_H__
