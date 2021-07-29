bc4ef3b618865796e66d521a6d8c32b8f7ec6ac6
Tirthankar Mazumder <63574588+wermos@users.noreply.github.com>
2021-07-29 20:52:16 +0530

    Made a copy constructor and a way to initialize BoundingVolume using an actual volume.

--- Physicc/src/boundingvolume.cpp
+++ Physicc/src/boundingvolume.cpp
@@ -2,6 +2,18 @@
 
 namespace Physicc
 {
+	template <class T>
+	BoundingVolume<T>::BoundingVolume(const BoundingVolume<T>& v)
+	{
+		m_volume = v.m_volume;
+	}
+
+	template <class T>
+	BoundingVolume<T>::BoundingVolume(const T& volume)
+	{
+		m_volume = volume;
+	}
+
 	template <>
 	bool BoundingVolume<AABB>::overlapsWith(BoundingVolume<AABB> v)
 	{
@@ -10,6 +22,7 @@ namespace Physicc
 			   (m_volume.lowerBound.z <= v.m_volume.upperBound.z && m_volume.upperBound.z >= v.m_volume.lowerBound.z);
 	}
 
-	template class BoundingVolume<AABB>;
+	template
+	class BoundingVolume<AABB>;
 	//Explicitly instantiating the AABB-based BoundingVolume class
 }
