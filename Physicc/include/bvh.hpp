b8b60da673898aca5168373cd91f1eb2e1ac8564
Tirthankar Mazumder <63574588+wermos@users.noreply.github.com>
2021-08-01 13:42:55 +0530

    Fleshed out the BVHNode struct.

--- Physicc/include/bvh.hpp
+++ Physicc/include/bvh.hpp
@@ -1,26 +1,34 @@
 #ifndef __BVH_H__
 #define __BVH_H__
 
+#include "boundingvolume.hpp"
+#include "rigidbody.hpp"
+
 namespace Physicc
 {
 	struct BVHNode
 	{
-
+		BoundingVolume<AABB> volume;
+		RigidBody* body = nullptr;
+		BVHNode* parent = nullptr;
+		BVHNode* left = nullptr;
+		BVHNode* right = nullptr;
 	};
 
 	class BVH
 	{
 		public:
-			BVH();
+			BVH(std::vector<RigidBody> rigidBodyList);
 
 			void buildTree();
 			//build a tree of the bounding volumes
 
-			std::vector<rididbody>& convert();
+			std::vector<RigidBody>& convert();
 			//convert the tree into a linear data structure
 
 		private:
 			BVHNode* m_head;
+			std::vector<RigidBody> m_rigidBodyList;
 	};
 }
 
