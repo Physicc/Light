2d161770c90d6a372b8492ed8652d7bc7eb800a5
Tirthankar Mazumder <63574588+wermos@users.noreply.github.com>
2021-08-27 01:01:48 +0530

    Added a getAABB() function to the RigidBody class.

--- Physicc/include/rigidbody.hpp
+++ Physicc/include/rigidbody.hpp
@@ -34,6 +34,9 @@ namespace Physicc
 
 			void setForce();
 
+			[[nodiscard]] BoundingVolume<AABB>  getAABB() {
+				return m_collider.getAABB();
+			}
 		private:
 			glm::vec3 m_force{};
 			BoxCollider m_collider{};
