ba1dbe4e33119397812faaa6875d72ea38857261
Tirthankar Mazumder <63574588+wermos@users.noreply.github.com>
2021-08-30 00:19:47 +0530

    Did some minor clean-up on the code and finished writing the getPotentialContactsWith() function.

--- Physicc/src/broadphase.cpp
+++ Physicc/src/broadphase.cpp
@@ -3,7 +3,6 @@
 
 namespace Physicc
 {
-
 	namespace Broadphase
 	{
 		struct PotentialContact
@@ -21,7 +20,6 @@ namespace Physicc
 		{
 			return (node->left == nullptr) && (node->right == nullptr);
 		}
-
 
 		void getPotentialContacts(BVHNode* node, std::vector<PotentialContact>& collisionArray) //TODO:Figure out a name for this array
 		{
@@ -42,25 +40,37 @@ namespace Physicc
 		{
 			if (isLeaf(node1) && isLeaf(node2))
 			{
-				//If both nodes are leaves then we have a PotentialContact and we add the collision pair to the collisionArray
+				//If both nodes are leaves, then we have a PotentialContact, and we add the collision pair to the collisionArray
 				collisionArray.push_back(PotentialContact(*(node1->body), *(node2->body)));
 			}
-			//If only of the nodes is a leaf, then make 
+			//If only of the nodes is a leaf, then make sure that node2 is the leaf node and that we recurse through node1
 			else if (isLeaf(node1) && !isLeaf(node2))
 			{
-				//recurse
-				getPotentialContactsWith(node2->left, node1, collisionArray); //
-				
-
+				getPotentialContactsWith(node2, node1, collisionArray);
+				//TODO: Discuss and figure out if the (minor?) performance penalty is worth it, or if we should just go for the following
+				/*
+				 * getPotentialContactsWith(node1, node2->left, collisionArray);
+				 * getPotentialContactsWith(node1, node2->right, collisionArray);
+				 */
 			}
 			else if (!isLeaf(node1) && isLeaf(node2))
 			{
-				
+				//recurse through both the left and right child of the non-leaf node (i.e. node1)
+				getPotentialContactsWith(node1->left, node2, collisionArray);
+				getPotentialContactsWith(node1->right, node2, collisionArray);
+			}
+			else {
+				/** Both nodes are non-leaf nodes */
+
+				//Recurse through node1, with node2 constant.
+				getPotentialContactsWith(node1->left, node2, collisionArray);
+				getPotentialContactsWith(node1->right, node2, collisionArray);
+
+				//Recurse through node2, with node1 constant.
+				getPotentialContactsWith(node1, node2->left, collisionArray);
+				getPotentialContactsWith(node1, node2->right, collisionArray);
 
 			}
-
-
-
 		}
 	}
 }
