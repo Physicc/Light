41251256fd195dcf2265f4447fbd17dd459acbd8
Tirthankar Mazumder <63574588+wermos@users.noreply.github.com>
2021-09-02 12:02:40 +0530

    Minor code style improvements and fixed a matching curly brace issue.

--- Physicc/include/boundingvolume.hpp
+++ Physicc/include/boundingvolume.hpp
@@ -43,7 +43,8 @@ namespace Physicc
 	 			 * @tparam BV The object to be copied
 	 			 * @param bv A BV object
 	 			 */
-				BV(const BV<Derived, BoundingObject>& bv) {
+				BV(const BV<Derived, BoundingObject>& bv)
+				{
 					typeCast()->setVolume(bv.typeCast()->m_volume);
 				}
 
@@ -55,11 +56,13 @@ namespace Physicc
 				 * (AABB, OBB, etc.)
 				 * @param volume
 				 */
-				BV(const BoundingObject& volume) {
+				BV(const BoundingObject& volume)
+				{
 					typeCast()->setVolume(volume);
 				}
 
-				BV(const glm::vec3& lowerBound, const glm::vec3& upperBound) {
+				BV(const glm::vec3& lowerBound, const glm::vec3& upperBound)
+				{
 					typeCast()->setVolume(lowerBound, upperBound);
 				}
 
@@ -70,13 +73,16 @@ namespace Physicc
  				 * @return true if the BoundingVolumes are intersecting, and false otherwise
  				 * TODO: Is this, as a return type description, fine?
  				 */
-				[[nodiscard]] inline bool overlapsWith(const BV<Derived, BoundingObject>& bv) {
+				[[nodiscard]] inline bool overlapsWith(const BV<Derived,
+				                                                BoundingObject>& bv)
+				{
 					return typeCast()->overlapsWith(static_cast<Derived&>(bv));
 				}
 				//implicit contract: all child classes of BV must have this
 				//function implemented
 
-				[[nodiscard]] inline float getVolume() {
+				[[nodiscard]] inline float getVolume()
+				{
 					return typeCast()->getVolume();
 				}
 				//Since template instantiations are lazy, a (child) class that
@@ -86,12 +92,12 @@ namespace Physicc
 				//errors.
 
 			private:
-				[[nodiscard]] inline Derived* typeCast() {
+				[[nodiscard]] inline Derived* typeCast()
+				{
 					return static_cast<Derived*>(this);
 				}
 				//A helper function just to make reading the code easier
 		};
-	}
 
 		template <typename T>
 		class BoxBV : public BV<BoxBV<T>, T>
@@ -102,17 +108,21 @@ namespace Physicc
 			public:
 				BoxBV() = default;
 
-				inline void setVolume(const T& volume) {
+				inline void setVolume(const T& volume)
+				{
 					m_volume = volume;
 				}
 
-				inline void setVolume(const glm::vec3& lowerBound, const glm::vec3& upperBound) {
+				inline void setVolume(const glm::vec3& lowerBound,
+				                      const glm::vec3& upperBound)
+				{
 					m_volume = T({lowerBound, upperBound});
 					//implicit contract: any BoxBV will have a struct that has a
 					//lowerBound and upperBound `glm::vec3`s.
 				}
 
-				inline float getVolume(BoxBV<T>& bv) {
+				inline float getVolume(BoxBV<T>& bv)
+				{
 					//[[nodiscard]] is not needed here because this function is
 					//never called by the end user. It is simply called by BV
 					//itself, which doesn't actually discard the return type, so
@@ -122,19 +132,27 @@ namespace Physicc
 						* (bv.upperBound.z - bv.lowerBound.z);
 				}
 
-				inline bool overlapsWith(BoxBV& bv) {
-					return (m_volume.lowerBound.x <= bv.m_volume.upperBound.x && m_volume.upperBound.x >= bv.m_volume.lowerBound.x)
-						&& (m_volume.lowerBound.y <= bv.m_volume.upperBound.y && m_volume.upperBound.y >= bv.m_volume.lowerBound.y)
-						&& (m_volume.lowerBound.z <= bv.m_volume.upperBound.z && m_volume.upperBound.z >= bv.m_volume.lowerBound.z);
+				inline bool overlapsWith(BoxBV& bv)
+				{
+					return (m_volume.lowerBound.x <= bv.m_volume.upperBound.x
+						&& m_volume.upperBound.x >= bv.m_volume.lowerBound.x)
+						&& (m_volume.lowerBound.y <= bv.m_volume.upperBound.y
+							&& m_volume.upperBound.y
+								>= bv.m_volume.lowerBound.y)
+						&& (m_volume.lowerBound.z <= bv.m_volume.upperBound.z
+							&& m_volume.upperBound.z
+								>= bv.m_volume.lowerBound.z);
 				}
 
 			private:
 				T m_volume;
 		};
+	}
 
 	namespace BoundingVolume
 	{
-		typedef BVImplementation::BV<BoxBV<BVImplementation::AABB>, BVImplementation::AABB> AABB;
+		typedef BVImplementation::BV<BVImplementation::BoxBV<BVImplementation::AABB>,
+		                             BVImplementation::AABB> AABB;
 	}
 }
 
