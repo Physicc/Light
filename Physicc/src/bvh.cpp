0914a09cff79c1d4bf388064e51e802e98775c9f
Tirthankar Mazumder <63574588+wermos@users.noreply.github.com>
2021-09-04 21:02:06 +0530

    Partially wrote the implementations of the functions declared in `bvh.hpp`.

--- Physicc/src/bvh.cpp
+++ Physicc/src/bvh.cpp
@@ -5,7 +5,6 @@ namespace Physicc
 {
 	using Iterator = std::vector<RigidBody>::iterator;
 
-	//TODO: Discuss the use of std::move
 	BVH::BVH(std::vector<RigidBody> rigidBodyList) : m_rigidBodyList(std::move(
 		rigidBodyList)), m_head(nullptr)
 	{
@@ -22,7 +21,7 @@ namespace Physicc
 
 		for (auto it = ++begin; it != end; ++it)
 		{
-			bv = BoundingVolume::minimumBoundingVolume(bv, it->getAABB());
+			bv = BoundingVolume::enclosingBV(bv, it->getAABB());
 		}
 
 		return bv;
@@ -33,5 +32,21 @@ namespace Physicc
 		//implicit convention:
 		//no children = leaf node
 		//no parent = head node
+
+		//All nodes are equal to nullptr until they are explicitly assigned
+		//non-null values
+
+		if (std::next(begin, 1) == end)
+		{
+			//then the only element left in this sliced vector is the one at
+			//`begin`
+			node->volume = begin->getAABB();
+			node->body = std::addressof(*begin);
+		} else
+		{
+			node->volume = BoundingVolume::AABB(computeBV(begin, end));
+
+			//next, we split
+		}
 	}
 }
