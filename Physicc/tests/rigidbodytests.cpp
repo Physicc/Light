fe984f24225ccf355302512ed82ad2d29156f214
Tirthankar Mazumder <63574588+wermos@users.noreply.github.com>
2021-09-08 22:24:10 +0530

    Minor code style improvements.

--- Physicc/tests/rigidbodytests.cpp
+++ Physicc/tests/rigidbodytests.cpp
@@ -1,12 +1,12 @@
 #include "gtest/gtest.h"
 #include "rigidbody.hpp"
 
-class RigidBodyTests : public ::testing::Test 
+class RigidBodyTests : public ::testing::Test
 {
     protected:
-        void SetUp() override 
+        void SetUp() override
         {
-            testBody = new Physicc::RigidBody(20,glm::vec3(1),1.0f);
+            testBody = new Physicc::RigidBody(20, glm::vec3(1), 1.0f);
         }
 
         void TearDown() override
@@ -19,11 +19,11 @@ class RigidBodyTests : public ::testing::Test
 
 TEST_F(RigidBodyTests, CheckVelocity)
 {
-    EXPECT_EQ(testBody->getVelocity(),glm::vec3(1.0)) << "Unable to initialize velocity";
+    EXPECT_EQ(testBody->getVelocity(), glm::vec3(1.0f)) << "Unable to initialize velocity";
 }
 
 TEST_F(RigidBodyTests, ChangeVelocity)
 {
-    testBody->setVelocity(glm::vec3(4.0));
-    EXPECT_EQ(testBody->getVelocity(),glm::vec3(4.0)) << "Unable to change velocity";
+    testBody->setVelocity(glm::vec3(4.0f));
+    EXPECT_EQ(testBody->getVelocity(), glm::vec3(4.0)) << "Unable to change velocity";
 }
