930447bd8744ef7fd85d7eb5e1d0d74e28ffccec
Prakhar Mittal <prakharmittal70@gmail.com>
2021-09-08 20:05:25 +0530

    Set up testing
    fix naming

---
+++ Physicc/tests/rigidbodytests.cpp
@@ -0,0 +1,29 @@
+#include "gtest/gtest.h"
+#include "rigidbody.hpp"
+
+class RigidBodyTests : public ::testing::Test 
+{
+    protected:
+        void SetUp() override 
+        {
+            testBody = new Physicc::RigidBody(20,glm::vec3(1),1.0f);
+        }
+
+        void TearDown() override
+        {
+            delete testBody;
+        }
+
+        Physicc::RigidBody* testBody;
+};
+
+TEST_F(RigidBodyTests, CheckVelocity)
+{
+    EXPECT_EQ(testBody->getVelocity(),glm::vec3(1.0)) << "Unable to initialize velocity";
+}
+
+TEST_F(RigidBodyTests, ChangeVelocity)
+{
+    testBody->setVelocity(glm::vec3(4.0));
+    EXPECT_EQ(testBody->getVelocity(),glm::vec3(4.0)) << "Unable to change velocity";
+}
