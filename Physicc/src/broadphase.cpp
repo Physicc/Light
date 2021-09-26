82c3dd3ee4389bd26acb6853a8cfccd0e8ccf94e
dropTableUsers42 <rwitaban0001@gmail.com>
2021-09-26 21:12:20 +0530

    Add TODO and open issues

--- Physicc/src/broadphase.cpp
+++ Physicc/src/broadphase.cpp
@@ -9,7 +9,7 @@ namespace Physicc
 
 		struct PotentialContact
 		{
-			PotentialContact(RigidBody& body1, RigidBody& body2) //TODO: Add a copy constructor to RigidBody in Development
+			PotentialContact(RigidBody& body1, RigidBody& body2) //TODO: #40 #39 @wermos Add a copy constructor to RigidBody in Development
 			: rb1(body1), rb2(body2)
 			{
 
@@ -30,9 +30,9 @@ namespace Physicc
 				return (node->left == nullptr) && (node->right == nullptr);
 			}
 
-			void getPotentialContactsWith(BVHNode* node1, BVHNode* node2, std::vector<PotentialContact>& collisionArray) //TODO: Figure out a name for the nodes here
-			{ //TODO: Discuss if there is any advantage to inlining this (i.e. using the `inline` keyword.)
-				//TODO: Check for the nullity of leaf nodes, and make sure that the function acts accordingly.
+			void getPotentialContactsWith(BVHNode* node1, BVHNode* node2, std::vector<PotentialContact>& collisionArray)
+			{
+				//TODO: #41 @divyanshtiwari237 Check for the nullity of leaf nodes, and assert condition
 				if (isLeaf(node1) && isLeaf(node2))
 				{
 					//If both nodes are leaves, then we have a PotentialContact, and we add the collision pair to the collisionArray
@@ -41,7 +41,7 @@ namespace Physicc
 				{
 					//If only of the nodes is a leaf, then make sure that node2 is the leaf node and that we recurse through node1
 					getPotentialContactsWith(node2, node1, collisionArray);
-					//TODO: Discuss and figure out if the (minor?) performance penalty is worth it, or if we should just go for the following
+					//TODO: #42 @dropTableUsers42 Discuss and figure out if the (minor?) performance penalty is worth it, or if we should just go for the following
 					/*
 					 * getPotentialContactsWith(node1, node2->left, collisionArray);
 					 * getPotentialContactsWith(node1, node2->right, collisionArray);
@@ -66,10 +66,8 @@ namespace Physicc
 				}
 			}
 
-			void getPotentialContacts(BVHNode* node, std::vector<PotentialContact>& collisionArray) //TODO:Figure out a name for this array
-			{ //TODO: Discuss if there is any advantage to inlining this (i.e. using the `inline` keyword.)
-				// LIGHT_
-
+			void getPotentialContacts(BVHNode* node, std::vector<PotentialContact>& collisionArray)
+			{
 				if (isLeaf(node))
 					return;
 
