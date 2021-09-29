554a6603bda778e11c3fbfad134c72dc287b353b
Prakhar Mittal <64589167+prak74@users.noreply.github.com>
2021-09-29 17:34:24 +0530

    Added getAABB test

--- Physicc/tests/rigidbody_tests.cpp
+++ Physicc/tests/rigidbody_tests.cpp
@@ -27,3 +27,15 @@ TEST_F(RigidBodyTests, ChangeVelocity)
     testBody->setVelocity(glm::vec3(4.0f));
     EXPECT_EQ(testBody->getVelocity(), glm::vec3(4.0)) << "Unable to change velocity";
 }
+
+TEST_F(RigidBodyTests, AABBTest)
+{
+    typedef Physicc::BVImpl::BoxBV<Physicc::BVImpl::AABB> BoxBV;
+    typedef Physicc::BVImpl::BaseBV<BoxBV,Physicc::BVImpl::AABB> BaseBV;
+    
+    BaseBV testAABB{glm::vec3(0),glm::vec3(0)};
+    float eps = 1e-7;
+    BaseBV testAABB2{glm::vec3(eps),glm::vec3(eps)};
+    EXPECT_EQ(testAABB2, testAABB) << "Incorrect AABB returned for RigidBody";
+    //std::cout<<"------------------"<<glm::all(glm::epsilonEqual(glm::vec3(0),glm::vec3(eps),1e-5f))<<"------------------\n";
+}
