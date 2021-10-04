84d709ddd76838780494cb773a389df192a8291f
dropTableUsers42 <rwitaban0001@gmail.com>
2021-10-04 20:54:33 +0530

    Add issue reference

--- LightFramework/src/platform/opengl/imgui_build.cpp
+++ LightFramework/src/platform/opengl/imgui_build.cpp
@@ -2,4 +2,4 @@
 #include "backends/imgui_impl_glfw.cpp"
 #include "backends/imgui_impl_opengl3.cpp"
 
-// TODO: @dropTableUsers42 Implement custom impl to remove direct interfacing with glad.h
+// TODO: #47 @dropTableUsers42 Implement custom impl to remove direct interfacing with glad.h
