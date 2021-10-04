57b87a85c885fd04626b1d171a01c6151bf11ebc
dropTableUsers42 <rwitaban0001@gmail.com>
2021-10-04 20:53:26 +0530

    Fixed faulty glad include in LightFramework

--- LightFramework/src/platform/opengl/imgui_build.cpp
+++ LightFramework/src/platform/opengl/imgui_build.cpp
@@ -1,3 +1,5 @@
 #define IMGUI_IMPL_OPENGL_LOADER_GLAD
 #include "backends/imgui_impl_glfw.cpp"
 #include "backends/imgui_impl_opengl3.cpp"
+
+// TODO: @dropTableUsers42 Implement custom impl to remove direct interfacing with glad.h
