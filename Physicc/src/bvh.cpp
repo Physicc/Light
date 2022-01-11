34ec5160add1a0b4a2bac35395c0b96f7c2abc2b
wermos <63574588+wermos@users.noreply.github.com>
2022-01-11 21:12:56 +0530

    Tweaked a variable name and used `nullptr` instead of `NULL`.

--- Physicc/src/bvh.cpp
+++ Physicc/src/bvh.cpp
@@ -153,24 +153,24 @@ namespace Physicc
 	{
 		std::stack<BVHNode*> s;
 		std::vector<RigidBody*> tree;
-		BVHNode* curr = m_head;
-		while(!s.empty() || curr != NULL)
+		BVHNode* currentNode = m_head;
+		while(!s.empty() || currentNode != nullptr)
 		{
 			
-			while(curr != NULL)
+			while(currentNode != nullptr)
 			{
-				s.push(curr);
-				curr = curr->left;
+				s.push(currentNode);
+				currentNode = currentNode->left;
 			}
 
-			curr = s.top();
+			currentNode = s.top();
 			s.pop();
 
-			if(curr->left == NULL && curr->right == NULL)
+			if(currentNode->left == nullptr && currentNode->right == nullptr)
 			{
-				tree.push_back(curr->body);
+				tree.push_back(currentNode->body);
 			}
-			curr = curr->right;
+			currentNode = currentNode->right;
 		}
 		return tree;
 	}
