bb7cdb79432ace9a14d98893f03c43b65dbab8bc
wermos <63574588+wermos@users.noreply.github.com>
2022-05-11 01:08:43 +0530

    Made the function pointer matrix code work.

--- Physicc/include/narrowphase.hpp
+++ Physicc/include/narrowphase.hpp
@@ -31,7 +31,7 @@ namespace Physicc::Narrowphase
 		// Friction has been removed as it is a property of rigid body and not of Contact
 	};
 
-	struct collisionFunctionMatrix; // forward declaration of the struct
+	class collisionFunctionMatrix; // forward declaration of the struct
 
 	class CollisionDetector
 	{
@@ -75,26 +75,58 @@ namespace Physicc::Narrowphase
 	// The collisionFunctionMatrix is defined here because it needs to be able to see the full
 	// definitions of all the functions before it can do its thing
 	template <typename... CollisionTypes>
-	struct collisionFunctionMatrix
-	{
-		typedef bool (*)(Broadphase::PotentialContact) collisionFuncSignature;
-		collisionFunctionMatrix()
-		{
-			// When an object of this struct is instatiated, we will contruct the matrix.
+	class collisionFunctionMatrix {
+		public:
+			typedef bool (*collisionFuncSignature)();
 
-		}
+			collisionFunctionMatrix() {
+				// When an object of this struct is instatiated, we will construct the
+				// matrix.
+				constructMatrix<0, 0, CollisionTypes...>();
+			}
 
-		constexpr std::array<std::array<collisionFuncSignature,
-				  sizeof...(CollisionTypes), sizeof...(CollisionTypes)>> matrix;
+			std::array<std::array<collisionFuncSignature, sizeof...(CollisionTypes)>,
+					   sizeof...(CollisionTypes)>
+				matrix;
 
-		private:
-		// We need a bunch of helper functions to construct the matrix
+			std::array<collisionFuncSignature, sizeof...(CollisionTypes)> operator[](std::size_t idx) {
+					return matrix[idx];
+			}
 
-		template <typename Head, typename... CollisionTypes, std::size_t row, std::size_t col>
-		constructRow() {
-			matrix[row][col] = 
-
-		}
+	   private:
+		  void constructMatrix() {
+			  matrix[row][col] = convert<Head, Head>;
+  
+			  if constexpr (sizeof...(Rest) > 0) {
+							  constructRow<row, col + 1, Head, Rest...>();
+							  constructCol<row + 1, col, Head, Rest...>();
+				  constructMatrix<row + 1, col + 1, Rest...>();
+			  } else {
+				  return;
+			  }
+		  }
+  
+		  template <std::size_t row, std::size_t col, typename Head, typename Next,
+				    typename... Rest>
+		  void constructRow() {
+			  matrix[row][col] = convert<Head, Next>;
+			  if constexpr (sizeof...(Rest) > 0) {
+				  constructRow<row, col + 1, Head, Rest...>();
+			  } else {
+				  return;
+			  }
+		  }
+  
+		  template <std::size_t row, std::size_t col, typename Head, typename Next,
+				    typename... Rest>
+		  void constructCol() {
+			  matrix[row][col] = convert<Next, Head>;
+			  if constexpr (sizeof...(Rest) > 0) {
+				  constructCol<row + 1, col, Head, Rest...>();
+			  } else {
+				  return;
+			  }
+		  }
 	};
 }
 
