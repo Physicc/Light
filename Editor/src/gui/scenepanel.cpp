646a8f2c3759733524b153372d5c294efcd087ba
PrrsnCrVS4sr <96426776+PrrsnCrVS4sr@users.noreply.github.com>
2022-05-17 13:32:29 +0530

    Changes to dragFloat sliders

--- Editor/src/gui/scenepanel.cpp
+++ Editor/src/gui/scenepanel.cpp
@@ -385,9 +385,10 @@ namespace Light
 				ImGui::SetNextItemWidth(fullWidth);
 				float width = fullWidth / 3.0f;
 				ImGui::SetNextItemWidth(width - lineHeight - 2.0f);
-				if(ImGui::DragFloat("##innerRad", &component.m_inner, 1.0f, 0.0f, component.m_outer, fmt))
+				if(ImGui::DragFloat("##innerRad", &component.m_inner, 1.0f, 0.0f, 90.0f, fmt))
 				{
-					component.m_inner = glm::clamp(component.m_inner, 0.0f, component.m_outer);
+					component.m_inner = glm::clamp(component.m_inner, 0.0f, 90.0f);
+					component.m_outer = glm::clamp(component.m_outer, component.m_inner, 90.0f);
 				}
 
 
@@ -405,9 +406,10 @@ namespace Light
 				ImGui::SetNextItemWidth(fullWidth);
 				width = fullWidth / 3.0f;
 				ImGui::SetNextItemWidth(width - lineHeight - 2.0f);
-				if(ImGui::DragFloat("##outerRad", &component.m_outer, 1.0f, component.m_inner, 90.0f, fmt))
-				{
-					component.m_outer = glm::clamp(component.m_outer, component.m_inner, 90.0f);
+				if(ImGui::DragFloat("##outerRad", &component.m_outer, 1.0f, 0.0f, 90.0f, fmt))
+				{	
+					component.m_outer = glm::clamp(component.m_outer, 0.0f, 90.0f);
+					component.m_inner = glm::clamp(component.m_inner, 0.0f, component.m_outer);
 				}
 				
 
