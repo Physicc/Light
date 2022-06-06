d9dc48aa6a032656c937bfcb2ca99393adb99ca3
wermos <63574588+wermos@users.noreply.github.com>
2022-06-07 00:34:55 +0530

    Fixed the MSVC compilation issue.

--- Light/src/platform/opengl/openglshader.cpp
+++ Light/src/platform/opengl/openglshader.cpp
@@ -49,6 +49,7 @@ namespace Light
 		}
 		catch (std::ifstream::failure& e)
 		{
+			(void)e; // Supress warning about unused variable in MSVC in release mode (logs don't get compiled in release)
 			LIGHT_CORE_ERROR("Shader file read failure:" + e.what());
 		}
 
